fb9b75e885b9c359d9185cc9f01e2d0ebbae1680
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2013-01-03 23:14:45 +0100

    Don't remove links on images
    Image links to other places should be respected (using
    `[[File:XY|link=target]]` syntax. Image links to the file descriptor are
    handled by the default link handler.

--- src/com/felixware/gw2w/utilities/Regexer.java
+++ src/com/felixware/gw2w/utilities/Regexer.java
@@ -6,15 +6,11 @@ import java.util.regex.Pattern;
 public class Regexer {
 	private static final Pattern redLinkPattern = Pattern.compile("<a href=\"[^\"]+?\" class=\"new\" title=\"[^\"]+?\">(.+?)<\\/a>");
 	private static final Pattern editSectionPattern = Pattern.compile("<span class=\"editsection\">.+?<\\/span>");
-	private static final Pattern fileLinkPattern = Pattern.compile("<a href=\"[^\"]+?\" class=\"image\">(.+?)<\\/a>");
-	private static final Pattern fileInclusionPattern = Pattern.compile("(<img alt=\"[^\"]+?\" src=\"([^\"]+?)\".+?/>)");
-	private static final Pattern imageUrlPattern = Pattern.compile("(.+?)thumb/(.+?)\\..+?\\.(.+?)");
 
 	// Wooo! Take it off!
 	public static String strip(String input) {
 		input = removeRedLinks(input);
 		input = removeEditSections(input);
-		input = removeFileLinks(input);
 
 		return input;
 	}
@@ -26,17 +22,4 @@ public class Regexer {
 	private static String removeEditSections(String input) {
 		return editSectionPattern.matcher(input).replaceAll("");
 	}
-
-	private static String removeFileLinks(String input) {
-		input = fileLinkPattern.matcher(input).replaceAll("$1");
-		return fileInclusionPattern.matcher(input).replaceAll("<a href=\"$2\">$1</a>");
-	}
-
-	public static String getImageUrl(String url) {
-		if (url.contains("thumb/"))
-			return imageUrlPattern.matcher(url).replaceFirst("$1$2.$3");
-		else
-			return url;
-	}
-
 }
